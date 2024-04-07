56fd2b92e4db66717447989c78387023a48663d5
Ruslan Zhytkovich <rzhytkovich@gmail.com>
2024-04-07 13:43:01 +0300

    Fixed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
-
 const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [accessToken, setAccessToken] = useState(null);
 
-  return (
-    isAuthenticated && ( 
-     <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        {/* {JSON.stringify(user, null, 2)} */}
-      </div>
-    )
-  )
-}
+    useEffect(() => {
+        const fetchAccessToken = async () => {
+            try {
+                const token = await getAccessTokenSilently();
+                setAccessToken(token);
+            } catch (error) {
+                console.error('Error fetching access token:', error);
+            }
+        };
 
-export default Profile
+        if (isAuthenticated) {
+            fetchAccessToken();
+        }
+    }, [isAuthenticated, getAccessTokenSilently]);
+
+    return (
+        isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+                <p>Access Token: {accessToken}</p>
+            </div>
+        )
+    );
+};
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
+const audience = process.env.REACT_APP_AUDIENCE;
 ReactDOM.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}>
-    <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+    <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          audience: audience,
+        }}
+        redirectUri={window.location.origin}
+        useRefreshTokens
+        cacheLocation="localstorage"
+    >
+      <App />
+    </Auth0Provider>,
+    document.getElementById('root')
 );
